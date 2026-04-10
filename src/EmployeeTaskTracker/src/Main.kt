8c7cb4fc6eec06d92437e16eff597d75620ee920
Dan <vfvf5389127@gmail.com>
2026-04-10 14:25:10 +0300

    Demonstrate equals, copyandtoString for Task

--- src/EmployeeTaskTracker/src/Main.kt
+++ src/EmployeeTaskTracker/src/Main.kt
@@ -12,4 +12,22 @@ fun main() {
     println("Должность: ${employee.getPosition()}")
     println("Зарплата: ${employee.getSalary()}")
     println("Опыт: ${employee.getYearsOfExperience()}")
+
+    // Задание 3
+    println("\n=== Демонстрация data-класса Task ===")
+
+    val task1 = Task("Fix bug", "Исправить ошибку в логине", Priority.HIGH)
+    val task2 = Task("Fix bug", "Исправить ошибку в логине", Priority.HIGH)
+    val task3 = Task("Write tests", "Написать юнит-тесты", Priority.MEDIUM)
+
+    println("task1 == task2: ${task1 == task2}")
+    println("task1 == task3: ${task1 == task3}")
+
+    println("\ntask1 hash code: ${task1.hashCode()}")
+    println("task2 hash code: ${task2.hashCode()}")
+
+    println("\ntask1 toString(): $task1")
+
+    val task4 = task1.copy(priority = Priority.LOW, isCompleted = true)
+    println("\nСкопированная задача с изменениями: $task4")
 }
